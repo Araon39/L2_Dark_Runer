a2159120ec8e3967b79debae8f3b8739ff3639f4
Araon39 <115640752+Araon39@users.noreply.github.com>
2024-12-16 21:14:02 +0200

    AddGameover
    Добавил касание двери и надпись игра окончена
    
    Добавил коментарии в код

--- Assets/Scripts/TagUp.cs
+++ Assets/Scripts/TagUp.cs
@@ -4,17 +4,23 @@ public class TagUp : MonoBehaviour
 {
     // Метод, вызываемый при столкновении с другим объектом
     private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("ArrowUp"))
-        {
-            transform.localScale += new Vector3(1f, 1f, 1f);
-            Destroy(collision.gameObject);                          // Уничтожение объекта, с которым произошло столкновение
+    {        
+        if (collision.gameObject.CompareTag("ArrowUp"))         // Проверка, если столкнувшийся объект имеет тег "ArrowUp"
+        {           
+            transform.localScale += new Vector3(1f, 1f, 1f);    // Увеличение размера объекта на 1 единицу по всем осям           
+            Destroy(collision.gameObject);                      // Уничтожение объекта, с которым произошло столкновение
         }
-
-        if (collision.gameObject.CompareTag("Obstacle"))
-        {
-            transform.localScale -= new Vector3(1f, 1f, 1f);
-            Destroy(collision.gameObject);                          // Уничтожение объекта, с которым произошло столкновение
+               
+        if (collision.gameObject.CompareTag("Obstacle"))        // Проверка, если столкнувшийся объект имеет тег "Obstacle"
+        {            
+            transform.localScale -= new Vector3(1f, 1f, 1f);    // Уменьшение размера объекта на 1 единицу по всем осям            
+            Destroy(collision.gameObject);                      // Уничтожение объекта, с которым произошло столкновение
         }
-    }   
+               
+        if (collision.gameObject.CompareTag("Door"))            // Проверка, если столкнувшийся объект имеет тег "Door"
+        {            
+            Debug.Log("-----GameOver------");                   // Вывод сообщения "-----GameOver------" в консоль
+        }
+    }
 }
+
